4e32864c88b5f62f25958eab5792f36e7dd1a9ba
Darkiane Lima <darkianelima13@gmail.com>
2025-04-15 17:01:39 -0300

    Criando o task e o começo da API REST.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 import User from "./model/user.js";
 salvar();
 async function salvar() {
@@ -7,3 +8,34 @@ async function salvar() {
     });
     console.log('User saved successfuly!')
 }
+*/
+
+/*
+import Task from "./model/task.js"
+*/
+
+import express from 'express';
+const app = express();
+app.use(express.json());
+const port = 3000
+
+import Task from "./model/task.js"
+
+app.get('/tasks', async (req, res)=>{
+    const tasks = await Task.findAll();
+    res.status(200).json(tasks);
+});
+
+app.post('/tasks', async(req, res)=>{
+    try{
+    const newTask = await Task.create(req.body);
+    res.status(2001).json(newTask);
+        }catch(err){
+            console.log(err);
+            res.status(400).json("Erro ao criar tarefa");
+        }
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});

295a16d2049df7946c9a1402afce08531d861064
Darkiane Lima <darkianelima13@gmail.com>
2025-04-28 16:04:09 -0300

    Index js

--- index.js
+++ index.js
@@ -17,25 +17,12 @@ import Task from "./model/task.js"
 import express from 'express';
 const app = express();
 app.use(express.json());
-const port = 3000
+const port = 3000;
 
-import Task from "./model/task.js"
+import TaskRouter from './router/task-router.js';
 
-app.get('/tasks', async (req, res)=>{
-    const tasks = await Task.findAll();
-    res.status(200).json(tasks);
-});
-
-app.post('/tasks', async(req, res)=>{
-    try{
-    const newTask = await Task.create(req.body);
-    res.status(2001).json(newTask);
-        }catch(err){
-            console.log(err);
-            res.status(400).json("Erro ao criar tarefa");
-        }
-});
+app.use('/tasks', TaskRouter);
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+  console.log(`App de exemplo esta rodando na porta ${port}`)
 });

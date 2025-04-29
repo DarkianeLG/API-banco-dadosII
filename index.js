04516d52e749881317428e0b7c0de8c390439e32
Darkiane Lima <darkianelima13@gmail.com>
2025-04-29 15:14:59 -0300

    ....

--- index.js
+++ index.js
@@ -15,14 +15,28 @@ import Task from "./model/task.js"
 */
 
 import express from 'express';
+import cors from 'cors';
 const app = express();
 app.use(express.json());
+app.use(cors());
 const port = 3000;
 
-import TaskRouter from './router/task-router.js';
+import TaskRouter from './router/tasks-router.js';
 
 app.use('/tasks', TaskRouter);
 
+//TODO: Refatorar
+import User from './model/user.js';
+app.post('/users', async (req,res)=>{
+    try{
+        const user = await User.create(req.body);
+        res.status(201).json(user);
+    }catch(err){
+        console.log(err);
+        res.status(400).json("Erro ao criar usuario");
+    }
+});
+
 app.listen(port, () => {
-  console.log(`App de exemplo esta rodando na porta ${port}`)
+    console.log(`App de exemplo esta rodando na porta ${port}`)
 });
